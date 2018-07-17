2538b4c5c0f5f24398bfa8326285842c5fc94c4a
BobWong <wangbo@bluemoon.com.cn>
2018-07-17 15:55:45 +0800

    Modify

---
+++ Web/Vue/to-do-list/src/components/MyView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    MyView
+    <div>{{titleToChange}}</div>
+    <button v-on:click="$emit('tapAction')">Invoke Parent Method</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyView',
+  props: {
+    titleToChange: {
+      type: String,
+      default: 'title to change'
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
